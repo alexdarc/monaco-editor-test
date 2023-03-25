4c1001e12e545d17cf316cdff07b737c15f281c8
Alexander Kim <alex.dark.kim@gmail.com>
2023-03-26 01:37:39 +0300

    init as vite project

---
+++ main.js
@@ -0,0 +1,31 @@
+import './style.scss'
+import * as monaco from 'monaco-editor';
+import { documentSource } from './document-source';
+
+const editor = monaco.editor.create(
+  document.getElementById('editor-container'),
+  {
+    value: documentSource,
+    language: 'html',
+  });
+
+
+editor.onMouseUp((evt) => {
+  if (evt.target.type === monaco.editor.MouseTargetType.CONTENT_TEXT
+    || evt.target.type === monaco.editor.MouseTargetType.CONTENT_EMPTY) {
+    const position = editor.getPosition();
+    document.getElementById('currentPositionLine').value = position.lineNumber;
+    document.getElementById('currentPositionColumn').value = position.column;
+  }
+})
+
+const editorPositionForm = document.getElementById('editorPositionForm');
+editorPositionForm.addEventListener('submit', (evt) => {
+  evt.preventDefault();
+  editor.setSelection({
+    startColumn: parseInt(editorPositionForm.elements.startColumn.value),
+    endColumn: parseInt(editorPositionForm.elements.endColumn.value),
+    startLineNumber: parseInt(editorPositionForm.elements.startLineNumber.value),
+    endLineNumber: parseInt(editorPositionForm.elements.endLineNumber.value),
+  });
+});
