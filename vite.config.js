e57198189bea4d2912c38d898f48921acaae110d
Alexander Kim <alex.dark.kim@gmail.com>
2023-03-26 04:23:10 +0300

    deploy

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,10 @@
 const path = require('path')
 
 export default {
+    base: '/monaco-editor-test/',
+    build: {
+        outDir: 'dist',
+    },
     resolve: {
         alias: {
             '~bootstrap': path.resolve(__dirname, 'node_modules/bootstrap'),
